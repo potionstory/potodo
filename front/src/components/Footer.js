98a77238460bf53b9505bdf35a4f07adc071549f
Potion Story <potionstory@gmail.com>
2018-05-12 23:17:23 +0900

    meterial css add

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,11 +1,44 @@
 import React, { Fragmnet } from 'react';
+import styled from 'styled-components';
 
-const Footer = (props) => {
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%; 
+  height: 50px;
+  padding: 5px;
+  text-align: center;
+  background-color: #ff8f00;
+  color: #fff;
+  small {
+    display: inline-block;
+    line-height:40px;
+  }
+  a {
+    width: 40px;
+    height: 40px;
+    padding: 0;
+    line-height: 40px;
+    opacity: 0.6;
+    i {
+      font-size: 2rem;
+    }
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+const FooterComponent = (props) => {
   return (
-    <div>
-      Footer
-    </div>
+    <Footer>
+      <small>Copyright @ 2018 potionstory. All rights reserved.</small>
+      <a className="right lighten-1 waves-effect waves-light btn-flat white">
+        <i className="material-icons amber-text text-darken-3">chat</i>
+      </a>
+    </Footer>
   );
 }
 
-export default Footer;
+export default FooterComponent;
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,11 +1,93 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Header = (props) => {
+const Header = styled.header`
+  height: 50px;
+  padding: 5px;
+  background-color: #ff8f00;
+  text-align: center;
+  i {
+    height: 40px;
+    line-height: 40px;
+    vertical-align: top;
+  }
+  h1 {
+    display: inline-block;
+    margin: 0;
+    height: 40px;
+    text-align: center;
+    a {
+      display: block;
+      height: 40px;
+      color: #fff;
+      font-size: 1.6rem;
+      line-height: 40px;
+      vertical-align: top;
+      opacity: 0.6;
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+  .left, .right {
+    ul {
+      margin: 0;
+      li {
+        float: left;
+        a {
+          width: 40px;
+          height: 40px;
+          padding: 0;
+          opacity: 0.6;
+          &:hover {
+            opacity: 1;
+          }
+          i {
+            font-size: 2rem;
+          }
+        }
+      }
+    }
+  }
+  .left {
+    li {
+      margin-right: 5px;
+    }
+  }
+  .right {
+    li {
+      margin-left: 5px;
+    }
+  }
+`;
+
+const HeaderComponent = (props) => {
   return (
-    <div>
-      Header
-    </div>
+    <Header>
+      <h1><Link to="/">TO<i className="material-icons">check_box</i>DO</Link></h1>
+      <div className="left">
+        <ul>
+          <li>
+            <a className="waves-effect waves-light btn-flat white"><i className="material-icons amber-text text-darken-3">info</i></a>
+          </li>
+          <li>
+            <a className="waves-effect waves-light btn-flat white"><i className="material-icons amber-text text-darken-3">star</i></a>
+          </li>
+        </ul>
+      </div>
+      <div className="right">
+        <ul>
+          <li>
+            <a className="waves-effect waves-light btn-flat white"><i className="material-icons amber-text text-darken-3">add</i></a>
+          </li>
+          <li>
+            <a className="waves-effect waves-light btn-flat white"><i className="material-icons amber-text text-darken-3">account_circle</i></a>
+          </li>
+        </ul>
+      </div>
+  </Header>
   );
 }
 
-export default Header;
+export default HeaderComponent;
